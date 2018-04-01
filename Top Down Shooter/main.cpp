bbca14f7a81010d4f8dc0da2703f06dd63d7f490
Niruz <niruz91@gmail.com>
2018-04-01 18:54:55 +0200

    Added window

--- Top Down Shooter/main.cpp
+++ Top Down Shooter/main.cpp
@@ -1,6 +1,187 @@
+#pragma once
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "opengl32.lib")
+
+#include <GL\glew.h>
+#include <glfw3.h>
 
 
-int main()
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <stdarg.h> 
+
+// glm::vec3, glm::vec4, glm::ivec4, glm::mat4
+#include <glm/glm.hpp>
+// glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/matrix_transform.hpp>
+// glm::value_ptr
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include <glm/glm.hpp> // vec3 normalize reflect dot pow
+#include <glm\gtx\vector_angle.hpp>
+
+float mHeight = 720.0f;
+float mWidth = 1280.0f;
+float aspectRatio = mWidth / mHeight;
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+bool keys[1024] = { false };
+
+bool firstMouse = true;
+
+GLfloat lastX = 640.0f;
+GLfloat lastY = 360.0f;
+glm::vec3 cameraPos = glm::vec3(0.0f);
+
+
+
+static void error_callback(int error, const char* description)
 {
-	return 0;
+	fputs(description, stderr);
+	_fgetchar();
 }
+
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	//cout << key << endl;
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keys[key] = true;
+		else if (action == GLFW_RELEASE)
+			keys[key] = false;
+	}
+}
+static void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;  // Reversed since y-coordinates go from bottom to left
+
+/*lastX = xpos;
+	lastY = ypos;
+
+	mCamera.processMouseMovement(xoffset, yoffset);*/
+}
+void updateInput(GLfloat deltaTime)
+{
+	// Camera controls
+/*	if (keys[GLFW_KEY_W])
+		mCamera.processKeyBoard(FORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		mCamera.processKeyBoard(BACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		mCamera.processKeyBoard(LEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		mCamera.processKeyBoard(RIGHT, deltaTime);
+	if (keys[GLFW_KEY_SPACE])
+		mCamera.processKeyBoard(UP, deltaTime);
+	if (keys[GLFW_KEY_LEFT_CONTROL])
+		mCamera.processKeyBoard(DOWN, deltaTime);*/
+}
+
+int main(void)
+{
+
+
+	//****************************************************//
+	//                                                    //
+	//               GLFW INITIALIZATION                  //
+	//                                                    //
+	//****************************************************//
+	if (!glfwInit())
+	{
+		exit(EXIT_FAILURE);
+	}
+
+	/*	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4); //Request a specific OpenGL version
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3); //Request a specific OpenGL version
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);*/
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+	int major;
+	int minor;
+	glfwGetVersion(&major, &minor, NULL);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(1280, 720, "Top Down Shooter", NULL, NULL);
+
+	glfwSetErrorCallback(error_callback);
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	//GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", glfwGetPrimaryMonitor(), NULL);
+	if (!window)
+	{
+		fprintf(stderr, "Failed to open GLFW window.\n");
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	//This function makes the context of the specified window current on the calling thread.   
+	glfwMakeContextCurrent(window);
+
+	//Sets the relevant callbacks 
+	//Initialize GLEW  
+	glewExperimental = true;
+	GLenum err = glewInit();
+
+	//If GLEW hasn't initialized  
+	if (err != GLEW_OK)
+	{
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		return -1;
+	}
+	//****************************************************//
+	//                                                    //
+	//             //GLFW INITIALIZATION                  //
+	//                                                    //
+	//****************************************************//
+
+	glViewport(0, 0, 1280, 720);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glEnable(GL_DEPTH_TEST);
+
+	do
+	{
+		// Update the input
+		GLfloat currentFrame = glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		updateInput(deltaTime);
+
+
+
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+
+
+		glfwSwapBuffers(window);
+		//Get and organize events, like keyboard and mouse input, window resizing, etc...  
+		glfwPollEvents();
+
+	} while (!glfwWindowShouldClose(window));
+
+
+	//Close OpenGL window and terminate GLFW  
+	glfwDestroyWindow(window);
+	//Finalize and clean up GLFW  
+	glfwTerminate();
+
+	exit(EXIT_SUCCESS);
+};
